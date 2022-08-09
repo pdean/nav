39b6d51fca7d55274da2b610f0f4d20e33a4fecb
Ted Paneer <tedpaneer@gmail.com>
2022-08-09 13:20:15 +1000

    sample now working!

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 #main.py
 
-from gevent import monkey
-monkey.patch_all()
-
-import time
+from datetime import datetime
 from threading import Thread
+from threading import Lock
 from flask import Flask, render_template, session, request
-from flask.ext.socketio import SocketIO, emit, join_room, disconnect
+from flask_socketio import SocketIO, emit, join_room, disconnect
 
 
 app = Flask(__name__)
@@ -14,34 +12,35 @@ app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
+thread_lock = Lock()
 
-
-def background_stuff():
-    """ Let's do it a bit cleaner """
+def background_thread():
+    print('started background task')
     while True:
-        time.sleep(1)
-        t = str(time.clock())
-        socketio.emit('message', {'data': 'This is data', 'time': t}, namespace='/test')
+        socketio.sleep(1)
+        now = datetime.now()
+        t = now.strftime("%H:%M:%S")
+        socketio.emit('message', {'data': 'This is data', 'time': t})
 
 
 @app.route('/')
 def index():
-    global thread
-    if thread is None:
-        thread = Thread(target=background_stuff)
-        thread.start()
     return render_template('index.html')
 
-@socketio.on('my event', namespace='/test')
+@socketio.on('my event')
 def my_event(msg):
-    print msg['data']
+    print( msg['data'])
 
-@socketio.on('connect', namespace='/test')
-def test_connect():
-    emit('my response', {'data': 'Connected', 'count': 0})
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+    emit('my_response', {'data': 'Connected', 'count': 0})    
 
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
 	
